6a12c57f08a7176cd99cd7b04e5ccd807a3c21da
Carrie Dils <carrie.dils-c@wpengine.com>
2023-05-10 14:24:06 -0500

    Revert "Merge pull request #1 from LinkedInLearning/05-03b"
    This reverts commit 404fc998ca8207f2691516b1b63ea206dc33ea34, reversing
    changes made to a4fc2eec28730afc91c7cba654defa28a1a6903a.

--- to-the-top.php
+++ to-the-top.php
@@ -1,1 +1,14 @@
 <?php
+/*
+ * Plugin Name:       To the Top
+ * Description:       Adds a 'back to top' link
+ * Version:           0.0.1
+ * Requires at least: 6.2
+ * Requires PHP:      8.0
+ * Author:            Carrie Dils
+ * Author URI:        https://carriedils.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       to-the-top
+ * Domain Path:       /languages
+ */
